6c26d01d539461fa171747d3757a7f6a041cdb6f
omidashtari <oashtari@gmail.com>
2022-05-23 08:12:39 -0700

    added wallet connectivity

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import React from "react";
-import HeadComponent from '../components/Head';
+// import HeadComponent from '../components/Head';
+
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
@@ -8,10 +12,22 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   
+  // This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
   
   return (
     <div className="App">
-      <HeadComponent/>
+      {/* <HeadComponent/> */}
       <div className="container">
         <header className="header-container">
           <p className="header"> 🐶 Omid's Puppy (picture) Emporium 🐶</p>
@@ -19,7 +35,8 @@ const App = () => {
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
